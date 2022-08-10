65d965d0210325460a687f9bef68547d1b8eb5db
mohammad sorooshfar <mohammad.sorooshfar@gmail.com>
2022-08-10 09:36:20 +0430

    add dashboard layout

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "pages/dashboard/Dashboard";
+import DashboardLayout from "layouts/dashboardLayout";
 const DashboardRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/products" element={<Dashboard />} />
-      <Route path="/inventory" element={<Dashboard />} />
-      <Route path="/orders" element={<Dashboard />} />
-    </Routes>
+    <DashboardLayout>
+      <Routes>
+        <Route path="/products" element={<Dashboard />} />
+        <Route path="/inventory" element={<Dashboard />} />
+        <Route path="/orders" element={<Dashboard />} />
+      </Routes>
+    </DashboardLayout>
   );
 };
 
