5895f73b9d9d19da584f53dcf3255f26dda61d79
mohammad sorooshfar <mohammad.sorooshfar@gmail.com>
2022-08-11 12:14:58 +0430

    add orders

--- src/utils/functions.util.ts
+++ src/utils/functions.util.ts
@@ -1,3 +1,5 @@
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "configs/variables.config";
+
 export const parseJwt = (token: string) => {
   let base64Url = token.split(".")[1];
   let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -11,3 +13,44 @@ export const parseJwt = (token: string) => {
   );
   return JSON.parse(jsonPayload);
 };
+export const CheckUserExpired = () => {
+  const token = localStorage.getItem(ACCESS_TOKEN);
+  if (!token) return;
+  const { exp } = parseJwt(token);
+  if (exp * 1000 < Date.now()) {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
+    return false;
+  }
+  return true;
+};
+
+export function convertMiladiToShamsi(gy: number, gm: number, gd: number) {
+  var g_d_m, jy, jm, jd, gy2, days;
+  g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+  gy2 = gm > 2 ? gy + 1 : gy;
+  days =
+    355666 +
+    365 * gy +
+    ~~((gy2 + 3) / 4) -
+    ~~((gy2 + 99) / 100) +
+    ~~((gy2 + 399) / 400) +
+    gd +
+    g_d_m[gm - 1];
+  jy = -1595 + 33 * ~~(days / 12053);
+  days %= 12053;
+  jy += 4 * ~~(days / 1461);
+  days %= 1461;
+  if (days > 365) {
+    jy += ~~((days - 1) / 365);
+    days = (days - 1) % 365;
+  }
+  if (days < 186) {
+    jm = 1 + ~~(days / 31);
+    jd = 1 + (days % 31);
+  } else {
+    jm = 7 + ~~((days - 186) / 30);
+    jd = 1 + ((days - 186) % 30);
+  }
+  return [jy + "/" + jm + "/" + jd];
+}
