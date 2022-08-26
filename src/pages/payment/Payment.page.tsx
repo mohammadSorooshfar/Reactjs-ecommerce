ee3f69a81b490717a74155fea6b1d682fb703236
mohammad sorooshfar <mohammad.sorooshfar@gmail.com>
2022-08-26 20:53:21 +0430

    add redirect in payment page

--- src/pages/payment/Payment.page.tsx
+++ src/pages/payment/Payment.page.tsx
@@ -1,21 +1,45 @@
-import { Container } from "@mui/material";
-import React from "react";
+import { Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import animationSuccessful from "assets/lotties/successful.json";
+import animationFailed from "assets/lotties/failed.json";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface props {}
 
 const Payment: React.FC<props> = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [duration, setDuration] = useState(5);
   const defaultOptions = {
-    loop: true,
+    loop: false,
     autoplay: true,
-    animationData: animationSuccessful,
+    animationData:
+      location.pathname === "/tehranshoes/pay/payment/failed"
+        ? animationFailed
+        : animationSuccessful,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+  useEffect(() => {
+    const timer = setInterval(
+      () => setDuration((prev: number) => prev - 1),
+      1000
+    );
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
-    <Container maxWidth={"lg"} sx={{ padding: 5 }}>
+    <Container
+      maxWidth={"lg"}
+      sx={{ padding: 5, display: "flex", justifyContent: "space-between" }}
+    >
+      <Typography variant="h6">{duration}</Typography>
       <Lottie options={defaultOptions} height={400} width={400} />
     </Container>
   );
